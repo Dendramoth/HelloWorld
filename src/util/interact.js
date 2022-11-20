ba14d79023491c50153d1f1960becbe80b4aae8e
Dendramoth <vitsi@seznam.cz>
2022-11-20 10:36:41 +0100

    Initial commit

---
+++ src/util/interact.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey); 
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x133027d1Fdb2ffd7884BFfFE6e3017E445464Ec5";
+
+export const helloWorldContract = new web3.eth.Contract(
+    contractABI,
+    contractAddress
+  );
+
+export const loadCurrentMessage = async () => { 
+    const message = await helloWorldContract.methods.message().call(); 
+    return message;
+};
+
+export const connectWallet = async () => {
+  
+};
+
+export const getCurrentWalletConnected = async () => {
+  
+};
+
+export const updateMessage = async (address, message) => {
+  
+};
